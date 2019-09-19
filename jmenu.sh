05b7d5f12967f25e2d540d2c313f53c361f52162
Benjamin Zacharias <benzach0421@gmail.com>
2019-09-19 00:48:58 -0600

    Add files via upload

---
+++ jmenu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bash -c "xsel -c --clipboard"
+
+main() {
+	cat ~/chars | dmenu -i -l 5 -fn Monospace-14 -p "$(xsel -o --clipboard)" | awk "{print $1}" | while IFS= read -r line
+	do
+		if [ "$line" == "*DEL" ]
+		then
+			str=`xsel -o --clipboard`
+			bash -c "echo '${str::-1}' | tr -d '\n' | xsel --clipboard"
+			main
+		else
+			bash -c "echo '$line' | tr -d ' [:alpha:]\n' | xsel -a --clipboard"
+			main
+		fi
+	done
+}
+main
+pgrep -x dunst >/dev/null && notify-send "$(xsel -o --clipboard) copied."
